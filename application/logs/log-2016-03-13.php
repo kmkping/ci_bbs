64887d393eb7f372264c995435943004f40c226c
Kyu Kim <kmkping1@gmail.com>
2016-03-13 20:05:18 +0900

    ci_practice

---
+++ application/logs/log-2016-03-13.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-03-13 04:02:29 --> Severity: Notice  --> Only variable references should be returned by reference C:\Bitnami\wampstack-5.6.19-0\apache2\htdocs\bbs\system\core\Common.php 257
+DEBUG - 2016-03-13 04:02:29 --> Config Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> Hooks Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> Utf8 Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> UTF-8 Support Enabled
+DEBUG - 2016-03-13 04:02:29 --> URI Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> Router Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> No URI present. Default controller set.
+DEBUG - 2016-03-13 04:02:29 --> Output Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> Security Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> Input Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> CRSF cookie Set
+DEBUG - 2016-03-13 04:02:29 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-03-13 04:02:29 --> Language Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> Loader Class Initialized
+DEBUG - 2016-03-13 04:02:29 --> Helper loaded: date_helper
+DEBUG - 2016-03-13 04:02:29 --> Database Driver Class Initialized
+ERROR - 2016-03-13 04:02:29 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\Bitnami\wampstack-5.6.19-0\apache2\htdocs\bbs\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2016-03-13 04:02:30 --> Severity: Warning  --> mysql_connect(): Access denied for user 'root'@'localhost' (using password: NO) C:\Bitnami\wampstack-5.6.19-0\apache2\htdocs\bbs\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2016-03-13 04:02:30 --> Unable to connect to the database
+DEBUG - 2016-03-13 04:02:30 --> Language file loaded: language/korean/db_lang.php
